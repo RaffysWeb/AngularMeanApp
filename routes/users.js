30c7d0e553055c533c97d5cab29e99e7cc3cd28c
raffy-dev <rsimoes.dev@gmail.com>
2018-01-18 22:18:01 +1100

    passport configured

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const config = require('../config/database');
 
 
 const User = require('../models/user');
@@ -33,13 +34,49 @@ router.post('/register', (req, res, next) => {
 });
 
 // Auth
-router.get('/authenticate', (req, res, next) => {
-  res.send('authenticate works')
+router.post('/authenticate', (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.getUserByUsername(username, (err, user) => {
+    if (err) throw err;
+    if (!user) {
+      return res.json({
+        success: false,
+        msg: 'Wrong credentials'
+      });
+    }
+
+    User.comparePassword(password, user.password, (err, isMatch) => {
+      if (err) throw err;
+      if (isMatch) {
+        const token = jwt.sign({data: user}, config.secret, {
+          expiresIn: 604800 //1 week
+        });
+        
+        res.json({
+          success: true,
+          token: 'JWT ' + token,
+          user: {
+            id: user._id,
+            user: user.name,
+            username: user.username,
+            email: user.email
+          }
+        });
+      } else {
+        return res.json({
+          success: false,
+          msg: 'Wrong credentials'
+        });
+      }
+    });
+  });
 });
 
 // Profile
-router.get('/profile', (req, res, next) => {
-  res.send('profile works')
+router.get('/profile', passport.authenticate('jwt', {session: false}), (req, res, next) => {
+ res.json({user: req.user});
 });
 
 

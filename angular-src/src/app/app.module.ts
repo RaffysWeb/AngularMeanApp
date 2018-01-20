0080e31bba6edfa658090793a22ccebf615f84fb
raffy-dev <rsimoes.dev@gmail.com>
2018-01-20 20:30:38 +1100

    Form validation added and flashMessages

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes} from '@angular/router';
-
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -10,13 +12,14 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { ValidateService } from './services/validate.service';
 
 const appRoutes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'dashboard', component: DashboardComponent},
-  {path: 'profile', component: ProfileComponent},
+  { path: '', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'profile', component: ProfileComponent },
 
 ];
 
@@ -32,9 +35,12 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-  RouterModule.forRoot(appRoutes)
-],
-  providers: [],
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    HttpModule,
+    FlashMessagesModule.forRoot()
+  ],
+  providers: [ValidateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { ValidateService } from '../../services/validate.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  user: User = {
+    name: undefined,
+    username: undefined,
+    email: undefined,
+    password: undefined
+  };
+
+  constructor(
+    private flashMessages: FlashMessagesService,
+    private validateService: ValidateService,
+  ) { }
+
 
   ngOnInit() {
+  }
+
+
+  onRegisterSubmit({ value, valid }: { value: User, valid: boolean }) {
+    if (!valid) {
+      this.flashMessages.show('Please fill in all fields', { cssClass: 'alert-danger', timeout: 3000 });
+      return false;
+    }
+    if (!this.validateService.validateEmail(value.email)) {
+      this.flashMessages.show('Please enter a valid email address', { cssClass: 'alert-danger', timeout: 3000 });
+      return false;
+    }
+    console.log(value);
   }
 
 }
